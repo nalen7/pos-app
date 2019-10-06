05022be220a1bd55c4d499d0aab50d58379d107d
nalen7 <nalen7@naver.com>
2019-10-06 19:58:16 +0900

    commit ---> pos-menutap-cdy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Menutap from './components/menutap.js';
+import Footer from './Footer.js';
 
 function App() {
   return (
     <div className="App">
           <div className='header'>Imform Cafe POS</div>
           <Menutap />
+          <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Footer extends React.Component {
+
+    render() {
+        return(
+            <div>
+                <div className='Footer'>
+                    (주)아이엠폼 CAFE-MENU [POS-APP]
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Footer;
