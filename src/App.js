96154b9a044bdebb633c00b16f7bb055da2ee555
Taei <gusld123@gmail.com>
2019-10-12 14:13:01 +0900

    TEST

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './App.css';
 import Menutap from './components/menutap.js';
 import Footer from './Footer.js';
+import Basket from './components/Basket';
 
 function App() {
   return (
+    <div>
     <div className="App">
           <div className='header'>Imform Cafe POS</div>
           <Menutap />
+    </div>
+    <div>
+      <div style={{float:"left"}} className="basket"><Basket/></div>
+      <div style={{float:"right"}}></div>
+    </div>
           <Footer />
     </div>
   );
