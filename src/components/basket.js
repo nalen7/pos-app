92f1fb92df202229fc709223707d1bbbf698812b
Taei <gusld123@gmail.com>
2019-11-05 12:26:31 +0900

    taei branch commit

--- src/components/basket.js
+++ src/components/basket.js
@@ -3,23 +3,33 @@ import '../css/basket.css';
 
 
 class Basket extends Component {
+   state={
+       basket:[]
+   }
+   componentDidUpdate(prevProps,prevState){
+       console.log(this.state.basket)
+       if(this.state.basket!==prevProps.basket){
+           this.setState({
+           basket:prevProps.basket
+       })
+   }}
+
     handleRemove=(id)=>{
-    console.log(id);
+
     }
     render(){
-        const basket=this.props;
         return (
             <div>
                 <div className="list-div"> <table>
                     <tbody>
                     <tr><th>상품명</th><th>가격</th><th>수량</th><th>삭제</th></tr>
-                    {basket.basket.map((item,index)=>(
+                      {this.state.basket.map((item,index)=>(
                         <tr key={index}><td>{item.name}</td>
                         <td>{item.price}</td>
                         <td>{item.qty}</td>
                         <td><span onClick={()=>this.handleRemove(item.id)}>삭제</span></td>
                         </tr>
-                    ))}
+                    ))}  
                     </tbody>
                     </table>
                     </div>
