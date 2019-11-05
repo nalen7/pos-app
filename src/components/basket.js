c3999d44b10547dbf0ccc1bfbd5d5e82fb1de008
nalen7 <nalen7@naver.com>
2019-11-05 12:26:20 +0900

    commit ---> pos-menutap-cdy     / 장바구니 리스트 생성

--- src/components/basket.js
+++ src/components/basket.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React,{Component} from 'react';
 import '../css/basket.css';
 
-function Basket(basket) {
-    basket.basket.map(item => {
-        console.log(item.name)
-    })
 
-    return (
-        <div>
-            <div className="list-div"> <table>
-                <tr><th>상품명</th><th>가격</th><th>수량</th><th>삭제</th></tr>
-            </table>
+class Basket extends Component {
+    state = {
+        basketAsync: [],
+    }
+    componentWillUpdate(prevProps, prevState, snapshot) {
+        if(prevProps.basket !== this.state.basketAsync){
+            this.setState({
+                basketAsync: prevProps.basket
+            });
+        }else{
+            return
+        }
+    }
+
+    handleDelete = (id) => {
+        console.log(id)
+    }
+    render(){
+        console.log('stateBasket', this.state.basketAsync)
+        return (
+            <div>
+                <div className="list-div"> <table>
+                    <tbody>
+                    <tr><th>상품명</th><th>가격</th><th>수량</th><th>삭제</th></tr>
+                    {this.state.basketAsync.map((item,index)=>(
+                        <tr key={index}><td>{item.name}</td>
+                            <td>{item.price}</td>
+                            <td>{item.qty}</td>
+                            <td><span onClick={() => this.handleDelete(item.id)}>삭제</span></td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+                </div>
+                <div className="price-div">PRICE</div>
             </div>
-            <div className="price-div">PRICE</div>
-        </div>
-    );
+        );
+    }
 }
+
 export default Basket;
